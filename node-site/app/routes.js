eed9880cd7f560f9e32d6396d1754b69b8eaa7b9
Vicky Chen <c.vicky1213@gmail.com>
2017-04-18 16:59:02 -0400

    more website features

--- node-site/app/routes.js
+++ node-site/app/routes.js
@@ -11,14 +11,28 @@ module.exports = router;
 
 //route for homepage
  router.get('/', function(req,res){
-	res.sendFile(path.join(__dirname,'../index.html'))
+	res.render('pages/index');
 })
 
 //route for about page
 router.get('/about', function(req,res){
-	res.sendFile(path.join(__dirname,'../about.html'))
+	var users =[
+		{name: "Running Dog", email:'swingdog@dog.com', avatar: 'http://r.ddmcdn.com/s_f/o_1/cx_633/cy_0/cw_1725/ch_1725/w_720/APL/uploads/2014/11/too-cute-doggone-it-video-playlist.jpg'}
+	]
+	res.render('pages/about', {users: users});
 })
 
 //route for contact
-router.get('/contact');
-router.post('/contact'); 
+router.get('/contact', function(req,res){
+	res.render('pages/contact') 
+})
+
+router.post('/contact', function(req,res){
+	res.send('Thanks for contacting us, ' + req.body.name + 'We will respond shortly!');
+}); 
+
+
+router.get("/:username", function( req, res) {
+	console.log(req.params);
+	res.send("You are the best " + req.params.username);
+});
